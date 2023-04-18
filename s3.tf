671604b653931027aa40134bc9110f3c304a615a
john-h-luo <jiehua.luo@zilliz.com>
2023-04-18 14:50:25 +0800

    Renaming bucket

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "test-s3-bucket-6785" {
+resource "aws_s3_bucket" "luotech-test-s3-bucket" {
   bucket = "luotech-test-s3-bucket"
   tags = {
     Environment = "test"
