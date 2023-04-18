8b3cf0236d63c439bd9bf8733a0d33e93c420274
john-h-luo <jiehua.luo@zilliz.com>
2023-04-18 16:30:30 +0800

    Add backend

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "luotech-terraform-aws-us-east-1"
+    key    = "tfbackend.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_s3_bucket" "luotech-test-s3-bucket" {
   bucket = "luotech-test-s3-bucket"
   tags = {
