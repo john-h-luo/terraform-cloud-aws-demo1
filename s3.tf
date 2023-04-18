7ea33598a09ce26eabb17f7e2d444cab4dcf2120
john-h-luo <jiehua.luo@zilliz.com>
2023-04-18 14:32:27 +0800

    Modify resource to be created

--- s3.tf
+++ s3.tf
@@ -1,3 +1,13 @@
-resource "aws_s3_bucket" "luotech-test" {
-  bucket = "luotech-test"
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "test-s3-bucket-6785"
+  acl = "private"
+  versioning {
+    enabled = true
+  }tags = {
+    Environment = "test"
+  }
 }
