6739cf8ca24349f7563a465488f5dbfaba91d47e
john-h-luo <jiehua.luo@zilliz.com>
2023-04-18 11:52:42 +0800

    Remove provider, var

--- provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-    region = var.AWS_REGION_US_WEST_2
-    profile = "dev"
-}
--- vars.tf
+++
@@ -1,6 +0,0 @@
-variable "AWS_REGION_US_WEST_2" {    
-    default = "us-west-2"
-}
-variable "AWS_REGION_US_EAST_2" {    
-    default = "us-east-2"
-}
